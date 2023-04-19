a0f8f473bb20746a171e694292d2e2bed9fd4658
jjjgm <businessjgm@icloud.com>
2023-04-18 19:01:42 -0700

    mm oh dont comment out fk's duh

--- models/index.js
+++ models/index.js
@@ -13,7 +13,7 @@ Product.belongsTo(Category,{
 
 // Categories have many Products
 Category.hasMany(Product,{
-  // foreignKey: 'category_id',
+  foreignKey: 'category_id',
 });
 
 // Products belongToMany Tags (through ProductTag)
